f1d1738ec636f51d862d080a043be52ccc779f4c
Jose Alanya <jose.alanya@gmail.com>
2021-05-31 20:53:58 -0500

    FEAT: Basics of Module Federation: Cart Integration

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   plugins: [
     new ModuleFederationPlugin({
       name: 'cart',
-      filename: 'remoteEntry',
+      filename: 'remoteEntry.js',
       exposes: {
         './CartShow': './src/index'
       }
